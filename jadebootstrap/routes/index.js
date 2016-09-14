3752e05a0e6c9f575ea5091c39f12bde661e1471
Bryce Barrand <barrand@gmail.com>
2016-09-14 14:54:39 -0400

    git ignore
    Updates to the layout and display

--- jadebootstrap/routes/index.js
+++ jadebootstrap/routes/index.js
@@ -4,14 +4,14 @@ var neo4j = require('neo4j-driver').v1;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log('get route');
 	var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"));
 	var session = driver.session();
 
   	
   	var names = [];
+  	var type = 'water'
 	session
-	  .run( "MATCH (Type1 { name:'fire' })<--(pokemon) RETURN pokemon.name" )
+	  .run( "MATCH (Type1 { name:'"+type+"' })<-[:type1|type2]-(pokemon) RETURN pokemon.name" )
 	  .subscribe({
 	    onNext: function(record) {
 	      names.push(record.get("pokemon.name"));
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
 	    },
 	    onCompleted: function() {
 	      session.close();
-	      res.render('index', { title: 'JADE-Bootstrap', names: names });
+	      res.render('index', { title: 'JADE-Bootstrap', names: names, type: type });
 	    },
 	    onError: function(error) {
 	      console.log(error);
