922c9604c9b04aa70dc652248f88ec9972069da2
Bryce Barrand <barrand@gmail.com>
2016-09-22 17:29:44 -0400

    almost got it. still need to get the owned

--- jadebootstrap/routes/index.js
+++ jadebootstrap/routes/index.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res, next) {
 	//   });
 });
 
-router.get('/saved', function(req, res, next) {
+router.get('/saved/', function(req, res, next) {
   	var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"));
 	var session = driver.session();
 
@@ -37,37 +37,88 @@ router.get('/saved', function(req, res, next) {
   	getAllPokemon(session, res);
 });
 
+router.get('/save/:id', function(req, res, next) {
+  	var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"));
+	var session = driver.session();
+	var id = req.params.id;
+  	session
+	  .run( "MATCH(p:Pokemon{pokemon_id:"+id+"}) CREATE (billy:User {name:'Billy'})-[:has_pokemon]->(p)" )
+	  .subscribe({
+	    onNext: function(record) {
+	    	
+	    },
+	    onCompleted: function() {
+	      console.log("just added " + id);
+	      session.close();
+	      res.redirect("/saved");
+	      //go to the saved page
+	    },
+	    onError: function(error) {
+	      console.log(error);
+	      session.close();
+	    }
+	  });
+});
+
 function getAllPokemon(session, res){
-	var toReturn = {};
-	toReturn['rare'] = [];
-	toReturn['occasional'] = [];
-	toReturn['common'] = [];
-	toReturn['everywhere'] = [];
+	var allPokemon = {};
+	allPokemon['rare'] = [];
+	allPokemon['occasional'] = [];
+	allPokemon['common'] = [];
+	allPokemon['everywhere'] = [];
 	session
 	  .run( "MATCH (p:Pokemon)-[:how_battle_common]->(c) RETURN p,c" )
 	  .subscribe({
 	    onNext: function(record) {
 	    	var pokemonObj = record.get("p")['properties'];
 			var howCommon =  record.get("c")['properties']['name'];
-			toReturn[howCommon].push(pokemonObj);
-			console.log("toReturn " + toReturn);
+			allPokemon[howCommon].push(pokemonObj);
 	    },
 	    onCompleted: function() {
-	      session.close();
-	      console.log("to return " + toReturn);
-	      renderSaved(toReturn, res);
+	      console.log("allPokemon " + allPokemon);
+	      getMyPokemon(session,res, allPokemon);
+	      // renderSaved(toReturn, res);
 	    },
 	    onError: function(error) {
 	      console.log(error);
+	      session.close();
 	    }
 	  });
 }
-function getMyPokemon(session){
-	return [];
+function getMyPokemon(session, res, allPokemon){
+	var myPokemonIds = [];
+	session
+	  .run( "MATCH (u:User {name:'Billy'}) MATCH (p:Pokemon)<-[:has_pokemon]-(u) RETURN p" )
+	  .subscribe({
+	    onNext: function(record) {
+	    	var pokemonObj = record.get("p")['properties'];
+			myPokemonIds.push(pokemonObj.pokemon_id);
+			console.log(pokemonObj.pokemon_id);
+	    },
+	    onCompleted: function() {
+	      console.log("to return " + myPokemonIds);
+	      renderSaved(allPokemon, myPokemonIds, res);
+	      // renderSaved(toReturn, res);
+	    },
+	    onError: function(error) {
+	      console.log(error);
+	      session.close();
+	    }
+	  });
 }
 
-function renderSaved(allPokemon, res){
-	res.render('saved', {rarePokemon: allPokemon['rare'], occasionalPokemon: allPokemon['occasional'], commonPokemon: allPokemon['common'], everywherePokemon: allPokemon['everywhere']});
+function renderSaved(allPokemon, myPokemonIds, res){
+	
+	for (p in allPokemon){
+		console.log("index " + myPokemonIds.indexOf(p.pokemon_id))
+		if(myPokemonIds.indexOf(p.pokemon_id) >= 0 ){
+			p.owned = true;
+			console.log("owned!!")
+		}else{
+			p.owned = false;
+		}
+	}
+	res.render('saved', {rarePokemon: allPokemon['rare'], occasionalPokemon: allPokemon['occasional'], commonPokemon: allPokemon['common'], everywherePokemon: allPokemon['everywhere'], myPokemonIds: myPokemonIds});
 }
 
 module.exports = router;
