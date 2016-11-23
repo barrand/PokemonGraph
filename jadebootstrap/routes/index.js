e9b6a5991b16f2bb91fb324897265e840d6411f6
Bryce Barrand <barrand@gmail.com>
2016-11-22 23:53:06 -0500

    Got the saving the first instance of the charge and fast moves. Also fixed the deleting of saved instances
    Still remaining is to make the connections happen in the server call

--- jadebootstrap/routes/index.js
+++ jadebootstrap/routes/index.js
@@ -38,11 +38,11 @@ router.get('/saveOpposing/:id', function(req, res, next) {
 
 router.get('/removeMine/:id', function(req, res, next) {
   	var id = req.params.id;
-  	removePokemon("has_pokemon", id, res);
+  	removePokemon("has_pokemon_instance", id, res);
 });
 router.get('/removeOpposing/:id', function(req, res, next) {
   	var id = req.params.id;
-  	removePokemon("opposing_pokemon", id, res);
+  	removePokemon("opposing_pokemon_instance", id, res);
 });
 
 function savePokemon(relationship, pokemon_id, res){
@@ -58,8 +58,35 @@ function savePokemon(relationship, pokemon_id, res){
 	    },
 	    onCompleted: function() {
 	      session.close();
+	      //Hard code something to add the first possible move set
+	      if(relationship == "has_pokemon"){
+	      	saveInstanceMoves(pokemon_id, res)
+	      //Otherwise just redirect to saved
+	      }else{
+	      	res.redirect("/saved");
+	      }
+	    },
+	    onError: function(error) {
+	      console.log(error);
+	      session.close();
+	    }
+	});
+}
+
+function saveInstanceMoves(pokemon_id, res){
+	var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"));
+	var session = driver.session();
+	var queryString = "MATCH(u:User{user_id:"+userId+"})-[:has_pokemon_instance]->(i:Pokemon_instance)-[:instance_of]->(p:Pokemon) MATCH (p)-[:has_fast_move]->(fm:Move)MATCH (p)-[:has_charge_move]->(cm:Move) WITH COLLECT(fm)[0] AS fastMoves, cm, u, pi WITH COLLECT(cm)[0] AS chargeMoves, fastMoves, u, pi MERGE (pi)-[:has_fast_move]->(fastMoves) MERGE (pi)-[:has_charge_move]->(chargeMoves) Return u, pi, fastMoves, chargeMoves";
+	console.log("queryString: "+queryString);
+  	session
+	  .run(queryString)
+	  .subscribe({
+	    onNext: function(record) {
+	    	
+	    },
+	    onCompleted: function() {
+	      session.close();
 	      res.redirect("/saved");
-	      //go to the saved page
 	    },
 	    onError: function(error) {
 	      console.log(error);
@@ -73,7 +100,7 @@ function removePokemon(relationship, pokemon_id, res){
 	var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "password"));
 	var session = driver.session();
   	session
-	  .run( "MATCH(p:Pokemon_instance{pokemon_id:"+pokemon_id+"}) MATCH (u:User {user_id:"+userId+"})-[r:"+relationship+"]->(p) DELETE r" )
+	  .run( "MATCH(pi:Pokemon_instance{pokemon_id:"+pokemon_id+"}) OPTIONAL MATCH (pi)-[r]-()WITH pi, r DELETE pi, r" )
 	  .subscribe({
 	    onNext: function(record) {
 	    	
@@ -105,7 +132,6 @@ function getAllPokemon(session, req, res){
 	    	var pokemonObj = record.get("p")['properties'];
 			var howCommon =  record.get("c")['properties']['name'];
 			allPokemon[howCommon].push(pokemonObj);
-			console.log("all pokemon " + pokemonObj.name);
 	    },
 	    onCompleted: function() {
 	      getMyPokemon(session, req, res, allPokemon);
@@ -130,7 +156,6 @@ function getMyPokemon(session, req, res, allPokemon){
 	    onNext: function(record) {
 	    	var pokemonObj = record.get("p")['properties'];
 			myPokemonIds.push(Number(pokemonObj.pokemon_id));
-			console.log("got my pokemons " +  pokemonObj.pokemon_id);
 			myPokemon.push(pokemonObj);
 	    },
 	    onCompleted: function() {
